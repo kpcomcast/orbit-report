28a88a40283e082788328c3b68e57fa392d68ee3
Kiran Poulsen <kiran_poulsen@comcast.com>
2021-06-17 16:17:43 -0600

    assignment 6 added satellite counts

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-counts',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrbitCountsComponent implements OnInit {
 
-  constructor() { }
+  @Input() satellites: Satellite[];
 
+  totalsObject: object;
+  
+  constructor() { }
+  
   ngOnInit() {
+  }
+
+  totalSat(array: Satellite[]): boolean {
+    this.totalsObject = {
+      total: array.length,
+      debris: 0,
+      comms: 0,
+      probe: 0,
+      position: 0,
+      station: 0,
+      tele: 0
+    };
+    
+    for (let thing of array) {
+      if (thing.type === "Space Debris") {
+        this.totalsObject["debris"] += 1;
+      }
+      if (thing.type === "Communication") {
+        this.totalsObject["comms"] += 1;
+      }
+      if (thing.type === "Probe") {
+        this.totalsObject["probe"] += 1;
+      }
+      if (thing.type === "Positioning") {
+        this.totalsObject["position"] += 1;
+      }
+      if (thing.type === "Space Station") {
+        this.totalsObject["station"] += 1;
+      }
+      if (thing.type === "Telescope") {
+        this.totalsObject["tele"] += 1;
+      }
+    }
+    return true;
   }
 
 }
